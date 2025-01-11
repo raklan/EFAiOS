379791b0f7768e0dc4bd600d46ff7967098ce1f8
raklan <rcl.catcher@gmail.com>
2025-01-11 15:00:48 -0700

    Messing with options

--- escape-api/main.go
+++ escape-api/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	registerPathHandlers()
+	fs := http.FileServer(http.Dir("./escape-api/assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	http.HandleFunc("/", serveHtml)
+
+	http.ListenAndServe(":80", nil)
+}
+
+func registerPathHandlers() {
+	//http.HandleFunc("/", escape_html.Test)
+}
+
+func serveHtml(w http.ResponseWriter, r *http.Request) {
+	//lp := filepath.Join("escape-api", "assets")
 }
