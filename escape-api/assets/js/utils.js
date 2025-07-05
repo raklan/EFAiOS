b339899857fcb8043cd868a3c84e467ba855625d
Ryan Lake <rcl.catcher@gmail.com>
2025-07-05 10:08:16 -0600

    Increase notification auto-close to 10 seconds

--- escape-api/assets/js/utils.js
+++ escape-api/assets/js/utils.js
@@ -50,7 +50,7 @@ function showNotification(notificationContent, notificationType) {
     if(notificationCloseTimeout){
         clearTimeout(notificationCloseTimeout)
     }
-    notificationCloseTimeout = setTimeout(hideNotification, 4000)
+    notificationCloseTimeout = setTimeout(hideNotification, 10000)
 }
 
 function showGameOver(){
